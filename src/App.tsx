f75a2cf77fae694e36280169219b48fecf9a68c6
ketarubot <robin09v@naver.com>
2025-11-02 18:17:29 +0900

    feat: 라우팅 구조 정립

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,67 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import * as pages from "./pages";
+import * as layouts from "./layouts";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* public */}
+        <Route path="/" element={<layouts.PublicLayout />}>
+          <Route index element={<pages.LandingPage />} />
+          {/* auth */}
+          <Route path="auth">
+            <Route path="login" element={<pages.LoginPage />} />
+            <Route path="register" element={<pages.RegisterPage />} />
+          </Route>
+        </Route>
+        {/* app */}
+        <Route path="/app" element={<layouts.AppLayout />}>
+          <Route index element={<pages.MainPage />} />
+          {/* class */}
+          <Route path="class">
+            <Route path="create" element={<pages.CreateClassPage />} />
+            <Route path="detail" element={<pages.ClassDetailPage />} />
+            <Route path="list" element={<pages.ClassListPage />} />
+            <Route path="manage" element={<pages.ManageClassPage />} />
+            <Route path="rate" element={<pages.RatePage />} />
+            <Route path="schedule" element={<pages.SchedulePage />} />
+            <Route path="assignment" element={<pages.SubmitAssignmentPage />} />
+          </Route>
+          {/* knowledge */}
+          <Route path="know">
+            <Route path="create" element={<pages.CreateKnowledgePage />} />
+            <Route path="detail" element={<pages.KnowledgeDetailPage />} />
+            <Route path="list" element={<pages.KnowledgeListPage />} />
+            <Route path="modify" element={<pages.ModifyKnowledgePage />} />
+            <Route path="search" element={<pages.SearchKnowledgePage />} />
+          </Route>
+          {/* mentoring */}
+          <Route path="mento">
+            <Route path="create" element={<pages.CreateRecruitmentPage />} />
+            <Route path="modify" element={<pages.ModifyRecruitmentPage />} />
+            <Route path="detail" element={<pages.RecruitmentDetailPage />} />
+            <Route path="list" element={<pages.RecruitmentListPage />} />
+            <Route path="search" element={<pages.SearchRecruitmentPage />} />
+          </Route>
+          {/* qna */}
+          <Route path="qna">
+            <Route path="create" element={<pages.CreateQnaPage />} />
+            <Route path="modify" element={<pages.ModifyQnaPage />} />
+            <Route path="detail" element={<pages.QnaDetailPage />} />
+            <Route path="list" element={<pages.QnaListPage />} />
+            <Route path="search" element={<pages.SearchQnaPage />} />
+          </Route>
+          {/* user */}
+          <Route path="user">
+            <Route path="my" element={<pages.MyPage />} />
+            <Route path="profile" element={<pages.EditProfilePage />} />
+            <Route path="posts" element={<pages.PostListPage />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
