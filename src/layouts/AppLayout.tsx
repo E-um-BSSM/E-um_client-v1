348ddc6eea3e33c145f3d5c29c6d2dd021f9c1eb
ketarubot <robin09v@naver.com>
2025-11-02 17:38:06 +0900

    feat: 레이아웃 추가

---
+++ src/layouts/AppLayout.tsx
@@ -0,0 +1,5 @@
+function AppLayout() {
+  return <></>;
+}
+
+export default AppLayout;
---
+++ src/layouts/index.ts
@@ -0,0 +1,4 @@
+import AppLayout from "./AppLayout";
+import PublicLayout from "./PublicLayout";
+
+export { AppLayout, PublicLayout };
