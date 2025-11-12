5cda3aaafbef089ada5e8365cce3fec17c06739f
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-12 21:12:41 +0900

    feat: class/schedule API 연결 함수 추가

---
+++ src/apis/class/schedule/index.ts
@@ -0,0 +1,34 @@
+import { req } from "../../axiosInstance";
+import type { classScheduleRequset, classScheduleResponse, globalResponse } from "../../../models/index";
+
+export const scheduleApi = {
+  classScheduleListSearch: async (class_id: number): Promise<globalResponse<object>> => {
+     const response = await req.get(`/classes/${class_id}/schedules`);
+     const data = {
+      schedules : response.data.data.schedules as classScheduleResponse[]
+     }
+     return {
+      ...response.data,
+      data
+     };
+  },
+  classScheduleRegister: async (class_id: number, body:classScheduleRequset): Promise<globalResponse<object>> => {
+     const response = await req.post(`/classes/${class_id}/schedules`,body);
+     return response.data;
+  },
+  classScheduleDelete: async (class_id: number,schedule_id:number): Promise<globalResponse<object>> => {
+    await req.delete(`/classes/${class_id}/schedules/${schedule_id}`);
+    return {
+      success: true,
+      data: {},
+    };
+  },
+  classScheduleUpdate: async (class_id: number,schedule_id:number, body:classScheduleRequset): Promise<globalResponse<classScheduleResponse>> => {
+     const response = await req.put(`/classes/${class_id}/schedules/${schedule_id}`,body);
+     return response.data;
+  },
+  classScheduleSingleSearch: async (class_id: number,schedule_id:number): Promise<globalResponse<classScheduleResponse>> => {
+     const response = await req.get(`/classes/${class_id}/schedules/${schedule_id}`);
+     return response.data;
+  },
+}
