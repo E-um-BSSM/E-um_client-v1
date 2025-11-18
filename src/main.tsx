5333c855eecba4b08bdd497e1eb30a204e686698
ketarubot <robin09v@naver.com>
2025-11-19 07:34:57 +0900

    chore: tokens, fonts css 파일 분리

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
-import "./index.css";
+import "./tokens.css";
+import "./fonts.css";
 
 const queryClient = new QueryClient();
 
