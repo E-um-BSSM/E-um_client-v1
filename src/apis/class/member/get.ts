97e11929f8d82a3c9ab53b7f0f8380584ce56054
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-19 00:33:13 +0900

    feat: member에서  코드 리팩토링 및 get API 함수 추가

--- src/apis/class/member/get.ts
+++ src/apis/class/member/get.ts
@@ -1,9 +1,9 @@
 import { req } from "@/apis/axiosInstance";
 import type { globalResponse } from "@/models";
 
-export const memberDELETE = {
-  memberDisagree: async (class_id: number): Promise<globalResponse<object>> => {
-    const response = await req.delete(`/classes/${class_id}/disagree`);
+export const memberGET = {
+  waitingListSearch: async (class_id: number): Promise<globalResponse<object>> => {
+    const response = await req.delete(`/classes/${class_id}/waiting`);
     return response.data;
   },
   memberKick: async (class_id: number): Promise<globalResponse<object>> => {
