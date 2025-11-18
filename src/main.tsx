81fc7622e013c64aef54a7f4bc3ff40d8821acbf
ketarubot <robin09v@naver.com>
2025-11-19 07:35:57 +0900

    feat: 전역 스타일 css 파일 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App.tsx";
 import "./tokens.css";
 import "./fonts.css";
+import "./global.css";
 
 const queryClient = new QueryClient();
 
