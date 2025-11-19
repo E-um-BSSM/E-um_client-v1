ec6555fb958b286541615e4e05a5b0e89674ff11
shusbox <shusbox@naver.com>
2025-11-19 15:33:06 +0900

    feat: push api 함수 추가

--- src/apis/post/post/patch.ts
+++ src/apis/post/post/patch.ts
@@ -3,7 +3,7 @@ import type { postRequest, postResponse, globalResponse } from "@/models";
 
 export const postPATCH = {
   postUpdate: async (post_id: number, body: postRequest): Promise<globalResponse<postResponse>> => {
-    const response = await req.patch(`/posts/${post_id}`, body);;
+    const response = await req.patch(`/posts/${post_id}`, body);
     return response.data;
   },
 };
