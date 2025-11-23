cd8f250706444aecc490031eac8757189fc6458e
shusbox <shusbox@naver.com>
2025-11-23 14:03:38 +0900

    refactor: transaction api 함수 수정

--- src/apis/transaction/post.ts
+++ src/apis/transaction/post.ts
@@ -1,14 +1,14 @@
 import { req } from "@/apis/axiosInstance";
-import type { globalResponse } from "@/models";
+import type { transferRequest, daliyCompensationReponse, globalResponse } from "@/models";
 
 export const transactionPOST = {
-  dailyCompensation: async (): Promise<globalResponse<{ currency: number }>> => {
+  dailyCompensation: async (): Promise<globalResponse<daliyCompensationReponse>> => {
     const response = await req.post(`/transaction/daily`);
     return response.data;
   },
 
-  giveCurrency: async (): Promise<globalResponse<object>> => {
-    const response = await req.post(`/transactions/transfer`);
+  giveCurrency: async (body: transferRequest): Promise<globalResponse<object>> => {
+    const response = await req.post(`/transactions/transfer`, body);
     return response.data;
   },
 };
