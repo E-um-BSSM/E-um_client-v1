74542c776d956f89f1e2acea1b1e4fface8cc924
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-27 08:47:46 +0900

    feat: DifficultyLevel 컴포넌트 추가

---
+++ src/components/DifficultyLevel/style.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const Difficulty = styled.div`
+  display: flex;
+  gap: 0.25rem;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  padding: 0.25rem 0;
+  flex-direction: row;
+  justify-content: center;
+  align-items: flex-start;
+  gap: 0.625rem;
+  color: var(--text-muted, #64748B);
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 1.25rem;
+  width: fit-content;
+`;
