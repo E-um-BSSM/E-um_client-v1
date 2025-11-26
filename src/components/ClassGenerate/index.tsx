7bc30b33f828ae5207aab20436e0fbe45c7a49fc
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-27 08:46:40 +0900

    feat: ClassGenerate 추가

---
+++ src/components/ClassGenerate/index.tsx
@@ -0,0 +1,10 @@
+import Class from "@/assets/class_natural-500.svg"
+
+import { Base } from "@/components/ClassGenerate/style";
+
+export default function ClassGenerate() {
+  return (
+    <Base>
+      <img src={Class} alt="Docs"/>클래스 개설하기</Base>
+  );
+}
