83b6c4afe816eb7ccae4271982220f45b3c8dbcb
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-11-26 12:37:28 +0900

    chore: 푸시 안된거

--- src/components/ClassGenerate/index.tsx
+++ src/components/ClassGenerate/index.tsx
@@ -2,7 +2,7 @@ import Class from "@/assets/class_natural-500.svg"
 
 import { Base } from "@/components/ClassGenerate/style";
 
-export default function WriteGenerate() {
+export default function ClassGenerate() {
   return (
     <Base>
       <img src={Class} alt="Docs"/>
