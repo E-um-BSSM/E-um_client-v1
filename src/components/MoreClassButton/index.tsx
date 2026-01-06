16180f9c9afaa01b5beaca36002e269715717cba
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-01-06 14:11:53 +0900

    feat:더보기 버튼 추가

---
+++ src/components/MoreClassButton/index.tsx
@@ -0,0 +1,11 @@
+/** @jsxImportSource @emotion/react */
+
+import { Base } from "@/components/MoreClassButton/styles";
+
+export default function MoreClassButton() {
+  return (
+    <Base>
+      더 많은 인기 클래스 보러가기
+    </Base>
+  );
+}
