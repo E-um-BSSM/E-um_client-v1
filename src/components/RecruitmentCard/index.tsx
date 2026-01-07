7e3b9b88c96b0341dfc63c0bfc7a13aff35b5c0a
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-01-07 17:53:34 +0900

    refactor:컴포넌트 호출 방식 수정

--- src/components/RecruitmentCard/index.tsx
+++ src/components/RecruitmentCard/index.tsx
@@ -1,13 +1,12 @@
 import { Container, Description, Info, Profile } from "@/components/RecruitmentCard/style";
-import RecruitmentDetailButton from "@/components/RecruitmentDetailButton";
-import DifficultyLevel from "@/components/DifficultyLevel";
+import {RecruitmentDetailButton, DifficultyLevel} from "@/components";
 
 export default function RecruitmentCard({ name, description, level }: { name: string; description: string; level: number }) {
   return (
     <Container>
       <Info>
         <Profile>
-          <p style={{ color: "var(--Primary-primary-500, #235FFF)" }}>멘토</p>
+          <p style={{ color: "var(--primary-500, #235FFF)" }}>멘토</p>
           <p>{name}</p>
         </Profile>
         <Description>{description}</Description>
