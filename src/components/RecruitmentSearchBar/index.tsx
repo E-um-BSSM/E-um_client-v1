b68c79d6d52f4a239eef3d18fdf0eb9316e7a798
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-01-26 15:26:46 +0900

    feat:RecruitmentSearchBar 컴포넌트 추가

---
+++ src/components/RecruitmentSearchBar/index.tsx
@@ -0,0 +1,13 @@
+/** @jsxImportSource @emotion/react */
+import { SearchBarContainer,Content,Text  } from "@/components/RecruitmentSearchBar/styles";
+import SearchSvg from "@/assets/Search_natural-500.svg";
+export function RecruitmentSearchBar() {
+ return(
+  <SearchBarContainer>
+   <Content>
+    <Text>멘토 이름, 카테고리, 난이도 별로 검색해요</Text>
+    <img src={SearchSvg} alt="search-icon" />
+   </Content>
+  </SearchBarContainer>
+ );
+}
