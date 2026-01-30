9a99e7b755d03f00f42c9b3d5fa4567d8161e317
shusbox <shusbox@naver.com>
2026-01-30 21:36:43 +0900

    feat: LandingPage 수정

--- src/pages/public/LandingPage/index.tsx
+++ src/pages/public/LandingPage/index.tsx
@@ -1,9 +1,16 @@
 import { useOutletContext } from "react-router-dom";
 import type { PageType } from "@/types/Page";
 import { useEffect } from "react";
+import {
+  Row,
+  Stack,
+  Title,
+  Text,
+  Button,
+  HeaderContainer,
+} from "./style";
 
 type PageTypeSetter = React.Dispatch<React.SetStateAction<PageType>>;
-
 function LandingPage() {
   const setPageType = useOutletContext<PageTypeSetter>();
 
@@ -11,7 +18,28 @@ function LandingPage() {
     setPageType("landing");
   }, [setPageType]);
 
-  return <></>;
+  function Header() {
+    return (
+      <HeaderContainer>
+        <Stack gap='20px' align="flex-start">
+          <Stack gap='8px' align="flex-start">
+            <Title> 작은 만남이 큰 경험으로 </Title>
+            <Row gap='4px' align='center'>
+              <Text color="muted" weight='regular'> 지식을 나누고 경험을 이어가는 멘토링 커뮤니티 플랫폼 </Text>
+              <Text color="highlight" weight='semibold'> 이음 </Text>
+            </Row>
+          </Stack>
+          <Button> 시작하기 </Button>
+        </Stack>
+      </HeaderContainer>
+    );
+  }
+
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default LandingPage;
