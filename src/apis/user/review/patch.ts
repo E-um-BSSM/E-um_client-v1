4623f2601365b7770a2745c5d28703e270f6f6dc
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-02-11 15:21:43 +0900

    refactor : user/review api 명세서와 불일치한 부분 수정

--- src/apis/user/review/patch.ts
+++ src/apis/user/review/patch.ts
@@ -3,7 +3,7 @@ import type { globalResponse, reviewRequest, reviewResponse } from "@/models";
 
 export const reviewPATCH = {
   reviewUpdate: async (reviewId: number, body: reviewRequest): Promise<globalResponse<reviewResponse>> => {
-    const response = await req.patch(`/reviews/${reviewId}`);
+    const response = await req.patch(`/reviews/${reviewId}`, body);
     return response.data;
   },
 };
