a7cf72150ca4b62cf0695c2dab96d0aa5fa0385c
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-02-11 15:21:07 +0900

    refactor : user/badge api 명세서와 불일치한 부분 수정

--- src/apis/user/badge/get.ts
+++ src/apis/user/badge/get.ts
@@ -3,7 +3,7 @@ import type { badgeResponse, globalResponse } from "@/models";
 
 export const bageGET = {
   badgeListSearch: async (): Promise<globalResponse<badgeResponse>> => {
-    const response = await req.get(`/users/badges`);
+    const response = await req.get(`/badges`);
     return response.data;
   },
 };
--- src/apis/user/badge/post.ts
+++ src/apis/user/badge/post.ts
@@ -3,15 +3,15 @@ import type { globalResponse } from "@/models";
 
 export const badgePOST = {
   badgeRegister: async (badgeId: string): Promise<globalResponse<Record<string, never>>> => {
-    const response = await req.post(`/users/badges/${badgeId}`);
+    const response = await req.post(`/badges/${badgeId}`);
     return response.data;
   },
   badgeEquip: async (badgeId: string): Promise<globalResponse<Record<string, never>>> => {
-    const response = await req.post(`/users/badges/${badgeId}/equip`);
+    const response = await req.patch(`/badges/${badgeId}/equip`);
     return response.data;
   },
   badgeRelease: async (badgeId: string): Promise<globalResponse<Record<string, never>>> => {
-    const response = await req.post(`/users/badges/${badgeId}/release`);
+    const response = await req.patch(`/badges/${badgeId}/release`);
     return response.data;
   },
 };
