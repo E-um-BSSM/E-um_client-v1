e8702680068ef4ba318acc77af4c70e59867defe
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-02-11 17:00:36 +0900

    refactor: API 타입 네이밍 정리

--- src/apis/class/schedule/post.ts
+++ src/apis/class/schedule/post.ts
@@ -1,10 +1,10 @@
 import { req } from "@/apis/axiosInstance";
-import type { classScheduleIdResponse, classScheduleRequset, globalResponse } from "@/models";
+import type { classScheduleIdResponse, classScheduleRequest, globalResponse } from "@/models";
 
 export const schedulePOST = {
   classScheduleRegister: async (
     class_id: number,
-    body: classScheduleRequset,
+    body: classScheduleRequest,
   ): Promise<globalResponse<classScheduleIdResponse>> => {
     const response = await req.post(`/classes/${class_id}/schedules`, body);
     return response.data;
