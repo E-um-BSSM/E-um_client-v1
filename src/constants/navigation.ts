f238a3ae767750ff165c0b22caf5afffbd7ed4e4
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-02-11 15:42:15 +0900

    refactor: 네비 상수 공통화

---
+++ src/constants/navigation.ts
@@ -0,0 +1,19 @@
+export const LANDING_NAV_ITEMS = {
+  "#MAIN": "메인",
+  "#FEATURE": "기능",
+  "#TOGETHER": "함께하기",
+  "#CONNECT": "이어가기",
+} as const;
+
+export const APP_NAV_ITEMS = {
+  "/app": "홈",
+  "/app/mento/list": "멘토-멘티",
+  "/app/community": "커뮤니티",
+  "/app/store": "상점",
+} as const;
+
+export const NAV_ITEMS = {
+  landing: LANDING_NAV_ITEMS,
+  app: APP_NAV_ITEMS,
+} as const;
+
