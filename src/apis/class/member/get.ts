b5916a6a898925f3359f19ba7d1eb9e4042cb977
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-02-12 18:33:39 +0900

    feat: MiniRecruitmentCard 추가

--- src/apis/class/member/get.ts
+++ src/apis/class/member/get.ts
@@ -2,7 +2,9 @@ import { req } from "@/apis/axiosInstance";
 import type { globalResponse, waitingListResponse } from "@/models";
 
 export const memberGET = {
-  waitingListSearch: async (class_id: number): Promise<globalResponse<waitingListResponse>> => {
+  waitingListSearch: async (
+    class_id: number,
+  ): Promise<globalResponse<{ content?: waitingListResponse[]; waiting?: waitingListResponse[] }>> => {
     const response = await req.get(`/classes/${class_id}/waiting`);
     return response.data;
   },
