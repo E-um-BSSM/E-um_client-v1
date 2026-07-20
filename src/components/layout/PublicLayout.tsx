1639e0f6616daea820cb48b99791e289c5666e35
ketarubot <robin09v@naver.com>
2026-07-21 01:43:50 +0900

    fix: 로그인 세션을 클래스 목록으로 이동

--- src/components/layout/PublicLayout.tsx
+++ src/components/layout/PublicLayout.tsx
@@ -1,17 +1,21 @@
 import { Header } from "@/components/layout/public";
-import { Outlet } from "react-router-dom";
-import { useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import type { PageType } from "@/types/Page";
+import { useAuthStore } from "@/stores";
 
 function PublicLayout() {
   const defaultPageType: PageType = "public";
   const [pageType, setPageType] = useState(defaultPageType);
-  // 홈 화면(/app) 자동 이동은 현재 비활성화합니다.
-  // useEffect(() => {
-  //   if (isHydrated && isAuthenticated) {
-  //     navigate("/app", { replace: true });
-  //   }
-  // }, [isAuthenticated, isHydrated, navigate]);
+  const navigate = useNavigate();
+  const isHydrated = useAuthStore(state => state.isHydrated);
+  const isAuthenticated = useAuthStore(state => state.isAuthenticated);
+
+  useEffect(() => {
+    if (isHydrated && isAuthenticated) {
+      navigate("/app/user/class/list", { replace: true });
+    }
+  }, [isAuthenticated, isHydrated, navigate]);
 
   return (
     <div className="publicLayout">
